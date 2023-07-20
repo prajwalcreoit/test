c5aff16fcea810cbb9979db5cc4c9eabca06aacb
“prajwalzoozle” <prajwal.dixit@zoozle.in>
2023-07-20 15:16:24 +0530

    Add 404.html to customise 404 error

--- mainsite/mainsite/settings.py
+++ mainsite/mainsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@w7h7hg^8e+stou9=1##h6%kr*9j0n7j^31f9w!4ft@gri)7z!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
--- mainsite/medic/urls.py
+++ mainsite/medic/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path('wardfillings/<int:pk>', views.WardFillings.as_view(), name='ward_fillings'),
     path('addmed/', views.add_medicine, name='add_medicine'),
     path('doctors/', views.ListDoctors.as_view(), name='list_doctors'),
-    path('medicine/<int:pk>/', views.DetailMedicine.as_view(), name='detail_medicine'),
     path('medicine/', views.ListMedicine.as_view(), name='list_medicine'),
+    path('medicine/<int:pk>/', views.DetailMedicine.as_view(), name='detail_medicine'),
     path('editpat/<int:pk>/', views.EditPatient.as_view(), name='edit_patient'),
     path('delpat/<int:pk>/', views.DeletePatient.as_view(), name='delete_patient'),
 ]
