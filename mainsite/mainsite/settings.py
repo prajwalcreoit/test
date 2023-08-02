af24aafddbef3a111162f5d553118b77bcb524d3
prajwalzoozle <prajwal.dixit@zoozle.in>
2023-08-02 11:32:57 +0530

    Add custom exception handler
    Add status code to the response body.

--- mainsite/mainsite/settings.py
+++ mainsite/mainsite/settings.py
@@ -85,7 +85,8 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'EXCEPTION_HANDLER': 'medic.utils.custom_exception_handler'
 }
 
 WSGI_APPLICATION = 'mainsite.wsgi.application'
